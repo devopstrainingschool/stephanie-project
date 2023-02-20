b8f96b1b7bc1e1da1f70a466d4a81be9f3e49db0
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-02-19 19:46:57 -0600

    Add files via upload

---
+++ web-ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+    ami = "ami-0cc87e5027adcdca8"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.private-web.id}"
+    key_name = "anael1"
+    vpc_security_group_ids = ["${aws_security_group.sg-web.id}"]
+    user_data = <<-EOF
+                #!/bin/bash
+                yum install httpd -y
+                systemctl start httpd
+                systemctl enable httpd
+                echo "Hello, World2" > /var/www/html/index.html
+                EOF
+    user_data_replace_on_change = true            
+    tags = {
+        Name = "Web_ec2"
+       
+    }
+}
